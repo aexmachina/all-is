6ed205168236ada43bbf71e3c0ca44e6135f4d03
Simon Wade <simon.wade@gmail.com>
2015-09-07 11:51:02 +1000

    Update

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 var ghost = require('ghost');
 var path = require('path');
-var app = require('express')();
-require('basement/basics').init();
-var config = require('config-node');
+var configFile = path.join(__dirname, 'config.js');
 
-var appP = require('src/app').init();
-appP.then(function(app) {
-    return ghost({config: config.ghostConfig})
-      .then(function(ghostServer) {
-        app.use(ghostServer.rootApp);
-        ghostServer.start(app);
-      });
-  })
-  .catch(function(err) {
-    try { require('lugg')('main').error(err); }
-    catch(e) { console.error(err); }
-    process.exit(1);
+ghost({config: configFile})
+  .then(function(ghostServer) {
+    ghostServer.start();
   });
